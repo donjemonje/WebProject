bc0e224e3e430c23c3d01368ab5cb15e40534188
Daniel East <daniel@beatman.com>
2016-09-10 03:19:58 +0300

    * added Upload Post: Logic + UI

--- MySocialNetworkingSite/html/main.php
+++ MySocialNetworkingSite/html/main.php
@@ -23,27 +23,16 @@ session_start();
     <script src="../js/main.js"></script>
     <script src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.11.4/jquery-ui.min.js"></script>
     <script src="http://ajax.aspnetcdn.com/ajax/bootstrap/3.3.6/bootstrap.min.js"></script>
-
-
-
     <!--#endregion javascript-->
 
     <title>Social Networking Web Site</title>
 </head>
 
 <body>
-<!--&lt;!&ndash; main right col &ndash;&gt;-->
-<!--<div class="column col-sm-10 col-xs-11" id="main">-->
 
 <!-- top nav -->
 <div class="navbar navbar-blue navbar-static-top">
     <div class="navbar-header">
-        <!--<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">-->
-            <!--<span class="sr-only">Toggle</span>-->
-            <!--<span class="icon-bar"></span>-->
-            <!--<span class="icon-bar"></span>-->
-            <!--<span class="icon-bar"></span>-->
-        <!--</button>-->
         <a  class="navbar-brand logo">M&D</a>
     </div>
     <nav class="collapse navbar-collapse" role="navigation">
@@ -60,28 +49,9 @@ session_start();
             <li>
                 <a href="profile.html"><i class="glyphicon glyphicon-user"></i> Profile</a>
             </li>
-            <!--<li>-->
-                <!--<a href="#postModal" role="button" data-toggle="modal"><i class="glyphicon glyphicon-plus"></i> Post</a>-->
-            <!--</li>-->
-            <!--<li>-->
-                <!--<a href="#"><span class="badge">badge</span></a>-->
-            <!--</li>-->
-
-
         </ul>
         <ul class="nav navbar-nav navbar-right right-nav">
-            <!--<li class="dropdown">-->
-                <!--<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i></a>-->
-                <!--<ul class="dropdown-menu">-->
-                    <!--<li><a href="">More</a></li>-->
-                    <!--<li><a href="">More</a></li>-->
-                    <!--<li><a href="">More</a></li>-->
-                    <!--<li><a href="">More</a></li>-->
-                    <!--<li>-->
-                        <!--<a href="../index.html">LogOut</a>-->
-                    <!--</li>-->
-                <!--</ul>-->
-            <!--</li>-->
+
             <li>
                 <a href="../php/logout.php">LogOut</a>
             </li>
@@ -89,72 +59,37 @@ session_start();
     </nav>
 </div>
 <!-- /top nav -->
+
 <br><br><br>
     <div class="container">
-        <!--<div class="header clearfix">-->
-            <!--<h3 class="text-muted">My Social Network Site</h3>-->
-            <!--<nav>-->
-                <!--<ul class="nav nav-tabs">-->
-                    <!--<li role="presentation" class="active"><a href="#">Home</a></li>-->
-                    <!--<li role="presentation"><a href="profile.html">Profile</a></li>-->
-                    <!--<li role="presentation"><a href="#">Messages</a></li>-->
-
-                    <!--<li role="presentation"><a href="../index.html">LogOut</a></li>-->
-                <!--</ul>-->
-            <!--</nav>-->
-        <!--</div>-->
-
-    <!-- ************************************************************************ -->
         <div class="row">
             <!-- ************************************************* -->
-            <div class="thumbnail" id="firstThumbnail">
+            <div class="thumbnail" id="firstThumbnail" >
                 <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>
-                        <button id="avi">Button</button>
-                    </p>
+
+                    <div style="height: 250px;">
+                        <ul style="list-style-type: none; overflow: hidden; display: inline;">
+                            <li style="display: block; float: left;"><img src="../images/main/1425577_1283411931698966_2669253318689249575_n.jpg" alt="..."  width="40" align="left" style="max-height:100%"></li>
+                            <li style="display: block; float: left; padding-left: 16px; max-width: 490px ;">
+                                <div id="postTextField" class="editable" contentEditable=true data-ph="What's On Your Mind..." style="max-height:230px;overflow-y: scroll; width: 490px;"></div>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div style="height: 30px;">
+                        <ul style="list-style-type: none; overflow: hidden; display: inline;">
+                            <li style="display: block; float: left;"><input name="picOneUpload" type="file" accept="image/*" onchange="picUpload(this.files[0])" style="padding-top: 8px"></li>
+                            <li style="display: block; float: right; padding-left: 16px; max-width: 490px ;max-height: 300px; ">
+                                <a id="myLink" title="Add" href="#" onclick="uploadPostClicked()" style="display: block;padding-right: 27px;padding-top: 8px;text-align:right";>Post</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <!-- ************************************************* -->
-            <div class="thumbnail">
-                <div class="thumbnail">
-                    <img src="../images/main/1425577_1283411931698966_2669253318689249575_n.jpg" alt="..." height="40" width="40" align="left">
-                    <br />
-                    <p>Avi Nessimian</p>
-                </div>
-                <img src="../images/main/little-mermaid.jpg" alt="..." height="400" width="400">
-                <br />
-            </div>
-            <!-- ************************************************* -->
-            <div class="thumbnail">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>
-                        <a href="#" class="btn btn-primary" role="button">Button</a>
-                        <a href="#" class="btn btn-default" role="button">Button</a>
-                        <a href="#" class="btn btn-default" role="button">Button</a>
-                        <a href="#" class="btn btn-default" role="button">Button</a>
-                    </p>
-                </div>
-            </div>
-            <!-- ************************************************* -->
-            <div class="thumbnail">
-                <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>
-                        <a href="#" class="btn btn-primary" role="button">Button</a>
-                        <a href="#" class="btn btn-default" role="button">Button</a>
-                        <a href="#" class="btn btn-default" role="button">Button</a>
-                        <a href="#" class="btn btn-default" role="button">Button</a>
-                    </p>
-                </div>
-            </div>
-    <!-- ************************************************************************ -->
         </div>
     
 
         <footer class="footer">
-            <p>&copy; Company 2016</p>
+            <p>&copy; Meital & Daniel 2016</p>
         </footer>
 
     </div> <!-- /container -->
